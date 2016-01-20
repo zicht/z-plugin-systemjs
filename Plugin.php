e8cfb95869be4eb8e5abe8ec1a05f693edcb5a6b
Gerard van Helden <gerard@zicht.nl>
2016-01-20 14:55:15 +0000

    moved node package to git
    git-svn-id: svn://zon/projecten/sana001/branches/gerard-dev-jsfixes/systemjs@77412 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ Plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Tool\Plugin\Systemjs;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Zicht\Tool\Plugin as BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('systemjs')
+                    ->children()
+                        ->scalarNode('bundle_js')->defaultValue('/usr/local/bin/systemjs-bundle')->end()
+                        ->scalarNode('system_config')->defaultValue('system.conf.js')->end()
+                        ->scalarNode('root')->defaultValue('htmldev')->end()
+                        ->scalarNode('src')->defaultValue('src')->end()
+                        ->scalarNode('target')->defaultValue('javascript')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+    }
+}
